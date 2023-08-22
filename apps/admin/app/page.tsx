6fbd749e9895213e4daed10904d62c4836671457
syssys131 <syssys131@gmail.com>
2023-08-22 12:32:01 +0900

    [setting] admin 워크스페이스 설정

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { isUserLoadingState, useRecoilValue } from '@near/store';
+import { Header } from 'ui';
+
+export default function Page() {
+  const recoilTest = useRecoilValue(isUserLoadingState);
+
+  return (
+    <div className='flex flex-col gap-1 p-6 bg-green-700'>
+      <Header text='[Admin]' />
+      <span className='text-white'>
+        process.env.NODE_ENV:<b>{process.env.NODE_ENV}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_URL:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_URL}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY}</b>
+      </span>
+      <span className='text-white'>
+        isUserLoadingState(recoil):<b>{recoilTest + ''}</b>
+      </span>
+    </div>
+  );
+}
