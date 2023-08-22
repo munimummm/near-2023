baba36bc9213fcf59a9604419120a1ba6b542ab5
syssys131 <syssys131@gmail.com>
2023-08-22 12:32:11 +0900

    [setting] client 워크스페이스 설정

---
+++ apps/client/app/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useRecoilValue, userEmailState } from '@near/store';
+import { Header } from 'ui';
+
+export default function Page() {
+  const recoilTest = useRecoilValue(userEmailState);
+
+  return (
+    <div className='flex flex-col gap-1 p-6 bg-blue-700'>
+      <Header text='[Client]' />
+      <span className='text-white'>
+        process.env.NODE_ENV:<b>{process.env.NODE_ENV}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_URL:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_URL}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</b>
+      </span>
+      <span className='text-white'>
+        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY:
+        <b>{process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY}</b>
+      </span>
+      <span className='text-white'>
+        userEmailState(recoil):<b>{recoilTest + ''}</b>
+      </span>
+    </div>
+  );
+}
---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@near/config/tailwind/tailwind.config.js');
