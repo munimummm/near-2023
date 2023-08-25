b81b2eab4a6a0d0ec1d42642cb5709bfe0783e5b
munimummm <moom6857@naver.com>
2023-08-25 12:20:16 +0900

    [test] 커밋액션 permission변경 상황

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -4,7 +4,7 @@ import { Button } from 'ui';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Example/Buttont1',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
