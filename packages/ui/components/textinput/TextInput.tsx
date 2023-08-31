7a9b05b28a4d33d93b130e32d8b9160c0a53dcef
munimummm <moom6857@naver.com>
2023-08-31 09:58:05 +0900

    [feat] TextInput 컴포넌트 사이즈 추가

--- packages/ui/components/textinput/TextInput.tsx
+++ packages/ui/components/textinput/TextInput.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+// import IconTextInputError from '../../assets/icons/textinput/icon_textinput_error.svg';
+type InputType = {
+  [key: string]: string;
+};
 
 interface TextInputProps {
   placeholder?: string;
-  state: 'enabled' | 'error' | 'success' | 'disabled';
+  state: 'enabled' | 'error' | 'success' | 'disabled' | 'search';
+  borderRadius?: boolean;
+  size?: 'lg' | 'sm';
 }
+const CommonStyle =
+  'w-full h-full pl-5 pr-10 rounded-[4px] focus-outline-4 active:bg-gray-3 active:text-[#333333] ';
+const TextInputStyle = {
+  enabled:
+    'bg-gray-3 text-[#C8C8C8] focus:outline-theme-main focus:bg-white focus:text-[#333333]',
+  error: 'border-text-red border-2 focus:outline-text-red',
+  success: 'border-[#00AE46] border-2  focus:outline-[#00AE46]',
+  disabled: 'bg-[#F0F0F0] text-[#C8C8C8]',
+  search: 'pl-10 active:pr-10',
+};
+const InputSize: InputType = {
+  lg: 'w-[403px] h-[48px]',
+  sm: 'w-[380px] h-[40px]',
+};
 
-function TextInput({ placeholder, state }: TextInputProps) {
-  const commonStyle =
-    'w-full h-full pl-5 pr-10 rounded-[4px] active:bg-gray-3 active:text-[#333333]';
-
-  const TextInputStyle = {
-    enabled: 'bg-gray-3 text-[#C8C8C8] focus:bg-white focus:text-[#333333]',
-    error: 'border-text-red border-2 focus:border-text-red',
-    success: 'border-[#00AE46] border-2 focus:border-[#00AE46]',
-    disabled: 'bg-[#F0F0F0] text-[#C8C8C8]',
-  };
+function TextInput({
+  placeholder,
+  state,
+  borderRadius = false,
+  size,
+}: TextInputProps) {
   const stateIcon = () => {
     switch (state) {
       case 'success':
@@ -31,11 +46,15 @@ function TextInput({ placeholder, state }: TextInputProps) {
   };
 
   return (
-    <div className='relative w-[380px] h-10'>
+    <div className='relative w-[380px] h-10 '>
       <input
         type='text'
         placeholder={placeholder}
-        className={`${commonStyle} ${TextInputStyle[state ?? 'enabled']}`}
+        className={` ${CommonStyle}
+        ${TextInputStyle[state ?? 'enabled']}
+        ${InputSize[size ?? 'sm']}
+        ${borderRadius ? 'rounded-[60px]' : ''}
+        `}
         disabled={state === 'disabled'}
       />
       {stateIcon()}
