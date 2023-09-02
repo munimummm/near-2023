447d8523c52fcc1c4aecb0ec39a5714869530f33
syssys131 <syssys131@gmail.com>
2023-09-02 11:48:51 +0900

    [feat] 수정 완료 컴포넌트 export #57

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,4 +1,9 @@
 // component exports
 export * from './components/buttons/Button';
+export * from './components/checkbox/Checkbox';
+export * from './components/toggle/Toggle';
+export * from './components/tag/Tag';
+export * from './components/dropdown/Dropdown';
+export * from './components/profile/Profile';
 export * from './components/textinput/TextInput';
 export * from './assets/icons/textinput/icon_textinput_error.svg';
