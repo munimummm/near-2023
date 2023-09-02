3adddb952357bb038b761a926096e1dac75d2a46
syssys131 <syssys131@gmail.com>
2023-09-02 11:47:14 +0900

    [feat] profile 컴포넌트 수정 #57

--- packages/ui/components/profile/Profile.tsx
+++ packages/ui/components/profile/Profile.tsx
@@ -1,27 +1,26 @@
-import Image from 'next/image';
+'use client';
+
 import IconProfileImageDefault from '../../assets/icons/profile/icon_profile_default.svg';
 
 interface ProfileProps {
-  ProfileImageUrl: string;
+  // ProfileImageUrl?: string;
   nickname: string;
   daysAgo?: string;
 }
 
-const Profile = ({ ProfileImageUrl, nickname, daysAgo }: ProfileProps) => {
+const Profile = ({ nickname, daysAgo }: ProfileProps) => {
   return (
-    <div className='flex items-center w-24 gap-3 h-9'>
-      <Image
-        className='absolute top-0 left-0 rounded-full w-9 h-9'
-        src={ProfileImageUrl ?? IconProfileImageDefault}
-        alt='프로필 이미지 아이콘'
-      />
+    <div className='flex items-center gap-[11px]'>
+      <div className='rounded-full w-[38px] h-[38px] overflow-hidden'>
+        <IconProfileImageDefault className='object-cover w-full h-full' />
+      </div>
       <div className='flex flex-col gap-1'>
         <span className='text-sm font-bold text-black'>{nickname}</span>
-        {daysAgo ?? (
-          <span className='text-xs font-normal text-zinc-400'>
+        {daysAgo ? (
+          <span className='text-[10px] font-normal leading-tight text-[#B4B4B4]'>
             {daysAgo}일전
           </span>
-        )}
+        ) : null}
       </div>
     </div>
   );
