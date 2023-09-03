f29156fc1e2c75d20e96f306fafd035eb1fe43f6
munimummm <moom6857@naver.com>
2023-09-04 06:55:50 +0900

    [feat] Footer 컴포넌트 스토리북 추가

---
+++ apps/storybook/src/stories/footer/Footer.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Footer from 'ui/components/footer/Footer';
+
+const meta = {
+  title: 'Components/Footer',
+  component: Footer,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  parameters: {
+    nextjs: {
+      router: {
+        basePath: '/profile',
+      },
+    },
+  },
+};
