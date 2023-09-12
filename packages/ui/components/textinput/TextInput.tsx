5d8efe19d336ade86a8c12a6895a9605ad3916df
munimummm <moom6857@naver.com>
2023-09-13 08:31:15 +0900

    [fix] textinput 컴포넌트 사이즈,cancel 이벤트 수정

--- packages/ui/components/textinput/TextInput.tsx
+++ packages/ui/components/textinput/TextInput.tsx
@@ -1,42 +1,44 @@
+'use client';
+
 import IconError from '../../assets/icons/textinput/icon_error.svg';
 import IconSuccess from '../../assets/icons/textinput/icon_success.svg';
 import IconSearch from '../../assets/icons/textinput/icon_search.svg';
 import IconCancel from '../../assets/icons/textinput/icon_cancel.svg';
 import { useState } from 'react';
 
-type InputType = {
-  [key: string]: string;
-};
-
 export interface TextInputProps {
   placeholder?: string;
   state: 'enabled' | 'error' | 'success' | 'disabled' | 'search';
   borderRadius?: boolean;
-  size?: 'lg' | 'sm';
+  size?: 'lg' | 'md' | 'sm' | 'xs';
 }
+
 const CommonStyle =
-  'w-full h-full pl-5 pr-10 rounded-[4px] focus-outline-4 active:bg-gray-3 active:text-[#333333] ';
+  'w-full h-full pl-5 pr-10 rounded-[4px] focus-outline-4 active:bg-gray-3 active:text-[#333333] f ';
+
 const TextInputStyle = {
   enabled:
-    'bg-gray-3 text-[#C8C8C8] focus:outline-theme-main focus:bg-white focus:text-[#333333]',
+    'font-normal bg-gray-3 text-[#C8C8C8] focus:outline-theme-main focus:bg-white focus:text-[#333333]',
   error: 'border-text-red border-2 focus:outline-text-red',
   success: 'border-[#00AE46] border-2  focus:outline-[#00AE46]',
   disabled: 'bg-[#F0F0F0] text-[#C8C8C8]',
-  search: 'pl-[52px] active:pr-10',
+  search: 'pl-[52px] active:pr-10 focus:',
 };
-const InputSize: InputType = {
-  lg: 'w-[403px] h-[48px]',
-  sm: 'w-[380px] h-[40px]',
+
+const InputSize = {
+  lg: 'w-[403px] h-[48px] text-base',
+  md: 'w-[380px] h-[40px] text-sm',
+  sm: 'w-[416px] h-[32px] text-sm',
+  xs: 'w-[349px] h-[32px] text-sm',
 };
 
 function TextInput({
   placeholder,
   state,
   borderRadius = false,
-  size,
+  size = 'sm',
 }: TextInputProps) {
-  const [isFocused, setIsFocused] = useState(false);
-  // const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
   const stateIcon = () => {
     switch (state) {
@@ -51,19 +53,23 @@ function TextInput({
     }
   };
 
+  const handleCancelClick = () => {
+    setText('');
+  };
+
   return (
-    <div className='relative w-[380px] h-10  '>
+    <div className={`relative ${InputSize[size]} `}>
       <input
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
         type='text'
+        value={text}
         placeholder={placeholder}
-        className={` ${CommonStyle}
-        ${TextInputStyle[state ?? 'enabled']}
-        ${InputSize[size ?? 'sm']}
+        className={`
+        ${CommonStyle}
+        ${TextInputStyle[state]}     
         ${borderRadius ? 'rounded-[60px]' : ''}
         `}
         disabled={state === 'disabled'}
+        onChange={(e) => setText(e.target.value)}
       />
       <div
         className={`absolute inset-y-0 flex items-center 
@@ -71,14 +77,16 @@ function TextInput({
       >
         {stateIcon()}
       </div>
-      {state === 'search' && isFocused && (
-        <div className='absolute inset-y-0 flex items-center right-4'>
+      {text && state === 'search' && (
+        <button
+          className='absolute inset-y-0 flex items-center right-4'
+          onClick={handleCancelClick}
+        >
           <IconCancel />
-        </div>
+        </button>
       )}
     </div>
   );
 }
 
 export default TextInput;
-// cancel 이벤트 추가
