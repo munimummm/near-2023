5d0c8354beb2ef1f6ad2f07f1901101985323869
munimummm <moom6857@naver.com>
2023-09-12 20:48:07 +0900

    [fix] footer 컴포넌트 수정

--- packages/ui/components/footer/Footer.tsx
+++ packages/ui/components/footer/Footer.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Link from 'next/link';
 import Logo from '../logo/Logo';
+import Copyright from '../copyright/Copyright';
+
 interface FooterLinkProps {
   href: string;
   label: string;
@@ -7,7 +11,7 @@ interface FooterLinkProps {
 
 function FooterLink({ href, label }: FooterLinkProps) {
   return (
-    <Link href={href} className='py-4 pr-5 text-lg'>
+    <Link href={href} className='py-4 pr-[40px] text-lg'>
       {label}
     </Link>
   );
@@ -24,24 +28,19 @@ function Footer() {
   return (
     <footer className='bg-white '>
       <div className='flex items-start mb-4'>
-        <section className='flex justify-center pt-5 ml-[47px] mr-[59.5px] '>
+        <section className='flex justify-center pt-5 ml-[47px] mr-[54px] '>
           <Logo />
         </section>
         <nav className='flex flex-wrap items-start ml-auto font-normal text-theme-main_dark'>
           {links.map((link) => (
             <FooterLink key={link.href} href={link.href} label={link.label} />
           ))}
+          {/* 링크 수정예정 */}
         </nav>
       </div>
       <hr className='mb-4 border border-theme-main ' />
-      {/* 컴포넌트로 변경할 예정 */}
       <section className='text-center'>
-        <p className='text-base font-normal text-center text-text-gray'>
-          Copyright © 2023 고장난 로켓
-        </p>
-        <p className='text-base font-normal text-center text-text-gray'>
-          All Rights Reserved
-        </p>
+        <Copyright size='sm' />
       </section>
     </footer>
   );
