98ba2755861d494728544154288a9128390010ad
munimummm <moom6857@naver.com>
2023-09-17 16:12:25 +0900

    [fix] ImageBox 컴포넌트 수정

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -18,3 +18,4 @@ export * from './components/comments/CommentInput';
 export * from './components/logo/Logo';
 export * from './components/textarea/TextArea';
 export * from './components/textarea/CommentBox';
+export * from './components/imagebox/ImageBox';
