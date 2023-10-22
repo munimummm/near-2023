da660b305abb75a713ff0011963137c80e488bf6
munimummm <moom6857@naver.com>
2023-10-22 23:11:53 +0900

    [fix] footershadowbutton 반응형 수정

--- packages/ui/components/footer/FooterShadowButton.tsx
+++ packages/ui/components/footer/FooterShadowButton.tsx
@@ -4,8 +4,11 @@ import Button from '../buttons/Button';
 
 function FooterShadowButton() {
   return (
-    <div className='shadow-[0_4px_15px_0_rgba(128,128,128,0.1)] flex justify-end md:justify-center sm:justify-center gap-6 px-[45px] py-[26px] bg-white'>
-      <div className='flex items-center justify-center gap-6'>
+    <div
+      className='shadow-[0_4px_15px_0_rgba(128,128,128,0.1)] bg-white h-[126px] flex justify-center items-center
+     mobile:w-[480px] tablet:w-[768px] desktop:[1440px]  desktop:justify-end desktop:pr-20'
+    >
+      <div className='flex items-center justify-center gap-8 desktop:gap-10 '>
         <Button isRounded mode='Outline' size='lg' label='임시저장' />
         <Button isRounded mode='Main' size='lg' label='등록하기' />
         {/* 버튼 수정예정 */}
