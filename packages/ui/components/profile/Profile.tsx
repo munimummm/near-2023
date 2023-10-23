fd0657836815d10e5a91e3c2790693da65135764
OhMinWook <minugi72@gmail.com>
2023-10-23 16:48:08 +0900

    component profile fix px to rem

--- packages/ui/components/profile/Profile.tsx
+++ packages/ui/components/profile/Profile.tsx
@@ -12,20 +12,20 @@ interface ProfileProps {
 
 const Profile = ({ nickname, daysAgo }: ProfileProps) => {
   return (
-    <div className='flex items-center gap-[5px] md:gap-[11px]'>
-      <div className='rounded-full w-[38px] h-[38px] overflow-hidden'>
+    <div className='flex items-center gap-[0.313rem] md:gap-[0.688rem]'>
+      <div className='rounded-full w-[2.375rem] h-[2.375rem] overflow-hidden'>
         <IconProfileImageDefault className='md:hidden object-cover w-full h-full' />
       </div>
       <div className=''>
         <div className='md:relative z-0'>
           <IconProfileNotificationDot className={'hidden md:block absolute left-0 top-0.5'}/>
         </div>
-        <IconProfileMdLgDefault className={'hidden md:block bg-orange-400'}/>
+        <IconProfileMdLgDefault className={'hidden md:block'}/>
       </div>
       <div className='flex flex-col gap-1'>
         <span className='text-sm font-bold text-black'>{nickname}</span>
         {daysAgo ? (
-          <span className='text-[10px] font-normal leading-tight text-[#B4B4B4]'>
+          <span className='text-[0.625rem] font-normal leading-tight text-[#B4B4B4]'>
             {daysAgo}일전
           </span>
         ) : null}
