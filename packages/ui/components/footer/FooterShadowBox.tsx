039bef6d399a4fd6fe64fc9632850ca123609c1e
munimummm <moom6857@naver.com>
2023-10-27 10:26:44 +0900

    [feat]  FooterShadowBox 추가

---
+++ packages/ui/components/footer/FooterShadowBox.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+interface FooterShadowBoxProps {
+  children: ReactNode;
+}
+
+function FooterShadowBox({ children }: FooterShadowBoxProps) {
+  return (
+    <div
+      className='shadow-[0_4px_15px_0_rgba(128,128,128,0.1)] bg-white h-[126px] flex justify-center items-center
+     mobile:w-[480px] tablet:w-[768px] desktop:w-[1440px]  desktop:justify-end desktop:pr-20'
+    >
+      <div className='flex items-center justify-center'>{children}</div>
+    </div>
+  );
+}
+
+export default FooterShadowBox;
