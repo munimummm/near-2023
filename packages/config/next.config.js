622ace52ca991d53b857caece76e9bb5eb48cdec
munimummm <moom6857@naver.com>
2023-10-30 16:15:37 +0900

    [bug fix] svg 웹팩 설정 변경 #110

--- packages/config/next.config.js
+++ packages/config/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['ui', 'shared-lib', 'config'],
   webpack(config) {
+    // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
+    // into React components. See https://react-svgr.com/docs/next/
+
     // Grab the existing rule that handles SVG imports
+    // @ts-ignore - rules is a private property that is not typed
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg'),
     );
@@ -18,8 +22,8 @@ const nextConfig = {
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ['@svgr/webpack'],
       },
     );
