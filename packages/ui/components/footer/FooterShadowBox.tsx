943a09cf2ce263d8102b7e35bf38af132926feb3
munimummm <moom6857@naver.com>
2023-11-02 12:34:50 +0900

    [fix] footershadowbox 스타일 수정

--- packages/ui/components/footer/FooterShadowBox.tsx
+++ packages/ui/components/footer/FooterShadowBox.tsx
@@ -8,12 +8,14 @@ interface FooterShadowBoxProps {
 
 function FooterShadowBox({ children }: FooterShadowBoxProps) {
   return (
-    <div
-      className='shadow-[0_4px_15px_0_rgba(128,128,128,0.1)] bg-white h-[126px] flex justify-center items-center 
+    <div className='fixed bottom-0 left-0 z-20 w-full'>
+      <div
+        className='shadow-[0_4px_15px_0_rgba(128,128,128,0.1)] bg-white h-[126px] flex justify-center items-center 
       w-full 
        desktop:justify-end desktop:pr-20'
-    >
-      <div className='flex items-center justify-center'>{children}</div>
+      >
+        <div className='flex items-center justify-center'>{children}</div>
+      </div>
     </div>
   );
 }
