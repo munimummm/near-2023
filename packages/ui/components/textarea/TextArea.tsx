cc4e97492ae3db94636a3e84ca5dde193cd382b7
munimummm <moom6857@naver.com>
2023-11-06 13:41:44 +0900

    [fix] textarea 리액트훅폼 추가

--- packages/ui/components/textarea/TextArea.tsx
+++ packages/ui/components/textarea/TextArea.tsx
@@ -1,13 +1,27 @@
 'use client';
-import { ChangeEvent, useState } from 'react';
-// import { FocusEvent } from 'react';
 
-interface TextAreaProps {
+import {
+  useController,
+  Control,
+  FieldPath,
+  RegisterOptions,
+  FieldValues,
+} from 'react-hook-form';
+
+export type TControl<T extends FieldValues> = {
+  control?: Control<T>;
+  name: FieldPath<T>;
+  rules?: Omit<
+    RegisterOptions<T>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >;
+};
+
+interface TextAreaProps extends TControl<FieldValues> {
   placeholder?: string;
   maxLength?: number;
-  width: number;
-  height: number;
-  setParentExceeding?: (exceeding: boolean) => void;
+  size?: 'sm' | 'md' | 'lg';
+  isDisabled?: boolean;
 }
 
 const styles = {
@@ -15,68 +29,57 @@ const styles = {
   textareaDefault:
     'w-full h-full p-3 pb-8 text-sm text-left resize-none text-text-gray rounded bg-[#F9F9F9]',
   textareaFocused: 'focus:text-black focus:outline-theme-main',
-  textareaExceeding: 'focus:outline-text-red',
+  textareaExceeding: 'focus:outline-text-red outline-text-red',
   counterContainer: 'absolute right-2 bottom-3',
   counterText: 'text-sm text-black',
   exceedingMessageContainer: 'mt-[11px] ml-[11px] ',
   exceedingMessageText: 'text-sm text-text-red',
 };
+const WidthSize = {
+  sm: 'w-[435px]',
+  md: 'w-[720px]',
+  lg: 'w-[864px]',
+};
 
 function TextArea({
+  name,
+  control,
   placeholder,
-  maxLength,
-  width = 435,
-  height = 259,
-  setParentExceeding,
+  maxLength = 200,
+  size = 'sm',
+  isDisabled = false,
 }: TextAreaProps) {
-  const [value, setValue] = useState('');
-  const [textCount, setTextCount] = useState(0);
-  const [isFocused, setFocused] = useState(false);
-  const [isExceeding, setExceeding] = useState(false);
-
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const inputValue = e.target.value;
-    if (maxLength && inputValue.length > maxLength) {
-      setExceeding(true);
-      setParentExceeding?.(true);
-      return;
-    }
-    setExceeding(false);
-    setParentExceeding?.(false);
-    setValue(inputValue);
-    setTextCount(inputValue.length);
-  };
-
-  const handleFocus = () => {
-    setFocused(true);
-  };
-
-  const handleBlur = () => {
-    setFocused(false);
-  };
+  const { field } = useController({
+    name,
+    control,
+    rules: {
+      maxLength: {
+        value: maxLength,
+        message: `글자수는 ${maxLength}자까지 입력 가능합니다.`,
+      },
+    },
+  });
+  const maxText = field.value?.length >= maxLength;
 
   return (
-    <div style={{ width: `${width}px`, height: `${height}px` }}>
+    <div className={`h-[259px] ${WidthSize[size]}`}>
       <div className={styles.textareaContainer}>
         <textarea
+          {...field}
           className={`
-        ${styles.textareaDefault} 
-        ${isFocused ? styles.textareaFocused : ''} 
-        ${isExceeding ? styles.textareaExceeding : ''}`}
+            ${styles.textareaDefault}
+            ${maxText ? styles.textareaExceeding : styles.textareaFocused}
+          `}
           placeholder={placeholder}
-          value={value}
-          onChange={handleChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+          maxLength={maxLength}
+          disabled={isDisabled}
         ></textarea>
-        {isFocused && (
-          <div className={styles.counterContainer}>
-            <span
-              className={styles.counterText}
-            >{`${textCount} / ${maxLength}`}</span>
-          </div>
-        )}
-        {isExceeding && (
+        <div className={styles.counterContainer}>
+          <span className={styles.counterText}>
+            {`${field.value?.length ?? 0} / ${maxLength}`}
+          </span>
+        </div>
+        {maxText && (
           <div className={styles.exceedingMessageContainer}>
             <span className={styles.exceedingMessageText}>
               글자수는 {maxLength}자까지 입력 가능합니다.
