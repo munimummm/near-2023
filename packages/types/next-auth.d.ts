68ca09f6394bc0a7a22cd2fd5d0d94fce9122fd8
OhMinWook <minugi72@gmail.com>
2023-11-10 16:38:44 +0900

    auth 수정

--- packages/types/next-auth.d.ts
+++ packages/types/next-auth.d.ts
@@ -1,9 +1,20 @@
 import NextAuth, { DefaultSession } from "next-auth"
 
+// 개인 회원 & 보호소 회원
 declare module "next-auth" {
     interface Session {
       user: {
+        email: string
+        password: string
+        name: string
+        birth: string
+        phone: string
         address: string
+        detail_address: string
+        shelter_name: string
+        shelter_type: string
+        ceo_name: string
+        ceo_phone: string
       } & DefaultSession["user"]
     }
 }
