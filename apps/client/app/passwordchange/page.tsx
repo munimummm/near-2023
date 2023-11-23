259030b7f9b3a3dda0c89ea43e5a65cfaa8d1550
OhMinWook <minugi72@gmail.com>
2023-11-23 14:07:05 +0900

    commit

--- apps/client/app/passwordchange/page.tsx
+++ apps/client/app/passwordchange/page.tsx
@@ -5,27 +5,55 @@ import TextInput from "ui/components/textinput/TextInput"
 import { ButtonXL } from "ui/components/buttons/Button" 
 import { useRouter } from "next/navigation";
 import { useState } from 'react';
+import FooterShadowBox from "ui/components/footer/FooterShadowBox";
+// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Modal } from 'antd';
+
+interface FormValues {
+    currentpw?: string
+    changepw?: string
+    pwcheck?: string
+}
 
 const PasswordChange = () => {
+    // const supabase = createClientComponentClient();
     const router = useRouter();
-    const { control } = useForm({
+    const { control, handleSubmit } = useForm<FormValues>({
         defaultValues :{
-            
+            currentpw:"",
+            changepw:"",
+            pwcheck:""
         },
         mode: "onChange"
     });
 
-    const [color, setColor] = useState<boolean>(false)
+    const [color, setColor] = useState<boolean>(false);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
     const onClickPersonalPage = () => {
         router.push('/personalchange')
         setColor(true)
     }
 
+    const onClickSubmit = async (userData: FormValues, e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        console.log(userData);
+        // let {data, error } = await supabase
+        //     .from("password")
+
+        setIsModalOpen(true);
+        router.push('/');
+    }
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    
+
     return (
         <div>
             <div></div>
-            <form>
+            <form onSubmit={handleSubmit(onClickSubmit)}>
                 <div className="mobile:flex mobile:flex-col mobile:justify-between mobile:items-center mobile:h-[6.25rem] mobile:mt-[8.25rem] mobile:mb-[2.5rem] 
                                 tablet:h-[9.25rem] tablet:grid tablet:justify-items-start">
                     <div className="moblie:pb-[3rem] moblie:text-[1.25rem] mobile:font-bold tablet:font-bold
@@ -50,7 +78,7 @@ const PasswordChange = () => {
                                         desktop:mb-[1.625rem]">
                             <div>현재 비밀번호</div>
                             <div className="mobile:w-[26rem] desktop:w-[43.1875rem]">
-                                <TextInput control={control} borderRadius= {true} placeholder='현재 비밀번호를 입력하세요' name={'currentpw'}/>
+                                <TextInput control={control} borderRadius= {true} placeholder='현재 비밀번호를 입력하세요' name={'currentpw'} rules={{required:false}}/>
                             </div>
                         </div>
                         <div className="mobile:flex mobile:justify-center mobile:h-[5.75rem] mobile:grid moible:mb-[0.625rem]
@@ -58,22 +86,27 @@ const PasswordChange = () => {
                                         desktop:mb-[1.625rem]">
                             <div>변경 비밀번호 입력</div>
                             <div className="mobile:w-[26rem] desktop:w-[43.1875rem]">
-                                <TextInput control={control} borderRadius= {true} placeholder='변경할 비밀번호를 입력하세요' name={'currentpw'}/>
+                                <TextInput control={control} borderRadius= {true} placeholder='변경할 비밀번호를 입력하세요' name={'changepw'} rules={{required:false}}/>
                             </div>
                         </div>
                         <div className="mobile:flex mobile:justify-center mobile:h-[5.75rem] mobile:grid moible:mb-[0.625rem]
                                         desktop:mb-[1.625rem]">
                             <div>변경 비밀번호 확인</div>
                             <div className="mobile:w-[26rem] desktop:w-[43.1875rem]">
-                                <TextInput control={control} borderRadius= {true} placeholder='변경할 비밀번호를 입력하세요' name={'currentpw'}/>
+                                <TextInput control={control} borderRadius= {true} placeholder='변경할 비밀번호를 입력하세요' name={'pwcheck'} rules={{required:false}}/>
                             </div>
                         </div>
                     </div>
-                    <div className="mobile:flex mobile: mobile:h-[8rem] mobile:items-center mobile:justify-center
-                                    desktop:relative">
-                        <div className="desktop:absolute right-[5.0625rem]">
-                            <ButtonXL>변경하기</ButtonXL>
+                    <div>
+                       <FooterShadowBox>
+                            <ButtonXL type="submit">변경하기</ButtonXL>
+                       </FooterShadowBox>
+                       <Modal width={480} open={isModalOpen} footer={null}>
+                        <div className="mobile:flex flex-col mobile:items-center">
+                            <p className="mobile:my-[50px] mobile:font-bold">비밀번호가 정상적으로 변경되었습니다.</p>
+                            <ButtonXL onClick={handleOk}>확인</ButtonXL>
                         </div>
+                    </Modal>
                     </div>
                 </div>
             </form>
--- apps/client/app/withdraw/page.tsx
+++ apps/client/app/withdraw/page.tsx
@@ -5,19 +5,42 @@ import { useForm } from "react-hook-form";
 import { ButtonXL } from "ui/components/buttons/Button"
 import { Modal } from 'antd';
 import { useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import FooterShadowBox from "ui/components/footer/FooterShadowBox";
 
+interface FormValues {
+
+}
 
 const Withdraw = () => {
-    const { control } = useForm({
+    const supabase = createClientComponentClient();
+    const { control, handleSubmit } = useForm<FormValues>({
         defaultValues :{
             
         },
         mode: "onChange"
     });
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-    const onClickWithdraw = () => {
+    const onClickWithdraw = async (userData: FormValues, e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        console.log(userData);
+        
+        const { data: { user } } = await supabase.auth.getUser()
+        console.log(user);
+        
+        let { data, error } = await supabase.auth.admin.deleteUser(
+            'ee4cbbee-b099-47ad-bdd4-131e601fbf9d'
+        )
+
+        if(error instanceof Error) {
+            console.log(error);
+        }
+        if(data) {
+            console.log(data);
+        }
+
         setIsModalOpen(true);
     }
 
@@ -30,7 +53,7 @@ const Withdraw = () => {
     return (
         <div>
             <div></div>
-            <form>
+            <form onSubmit={handleSubmit(onClickWithdraw)}>
                 <div className="mobile:h-[3.125rem] mobile:w-[28.75rem] mobile:m-auto mobile:flex mobile:justify-center mobile:mt-[8.6875rem] mobile:font-bold mobile:text-[1.25rem] mobile:border-b-2
                                 tablet:flex tablet:justify-start tablet:pl-[77px] tablet:w-[740px] mobile:h-[50px]
                                 desktop:w-[1408px]
@@ -56,15 +79,14 @@ const Withdraw = () => {
                         <CheckBox label=" 안내 사항을 모두 확인하였으며, 이에 동의합니다." control={control} name={'withdraw'} labelType="singletext"/>
                     </div>
                 </div>
-                <div className="mobile:flex mobile: mobile:h-[8rem] mobile:items-center mobile:justify-center
-                                    desktop:relative">
-                    <div className="">
-                        <ButtonXL onClick={onClickWithdraw}>탈퇴하기</ButtonXL>
-                    </div>
+                <div>
+                   <FooterShadowBox>
+                    <ButtonXL type="submit">탈퇴하기</ButtonXL>
+                   </FooterShadowBox>
                     <Modal width={480} open={isModalOpen} footer={null}>
                         <div className="mobile:flex flex-col mobile:items-center">
                             <p className="mobile:my-[50px] mobile:font-bold">정상적으로 회원탈퇴가 진행되었습니다.</p>
-                            <ButtonXL onClick={handleOk}>변경하기</ButtonXL>
+                            <ButtonXL onClick={handleOk}>확인</ButtonXL>
                         </div>
                     </Modal>
                 </div>
