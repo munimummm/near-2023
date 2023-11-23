7f606c1ee9b4d66e531080e492390aba81830fdb
munimummm <moom6857@naver.com>
2023-11-23 11:18:34 +0900

    [fix] 바텀 캐러셀 구조변경

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -3,16 +3,17 @@
 // import { useRecoilValue, userEmailState } from '@near/store';
 import TopCarousel from '../components/home/TopCarousel';
 import BottomCarousel from '../components/home/BottomCarousel';
-import { Products } from '../components/home/dummy';
+import { TopData } from '../components/home/dummy';
+import { BottomData } from '../components/home/dummy';
 export default function Page() {
   // const recoilTest = useRecoilValue(userEmailState);
 
   return (
     <div className='flex justify-center'>
       <div className='layout_max_width '>
-        <TopCarousel slides={Products} />
-        <div className='w-full h-[1000px] bg-black'></div>
-        <BottomCarousel slides={Products} />
+        <TopCarousel slides={TopData} />
+        <div className='w-full h-[500px] bg-black'></div>
+        <BottomCarousel slides={BottomData} />
       </div>
     </div>
   );
