e8c02a8107219176955b25531f6d0cba7c12b205
munimummm <moom6857@naver.com>
2023-11-25 16:21:31 +0900

    [fix] footer 스타일 및 구조변경 #65

--- packages/ui/components/footer/Footer.tsx
+++ packages/ui/components/footer/Footer.tsx
@@ -5,22 +5,23 @@ import Logo from '../logo/Logo';
 import Copyright from '../copyright/Copyright';
 import { useInnerWidthState } from '@near/utils';
 
-interface FooterLinkProps {
-  href: string;
-  label: string;
+interface Menu {
+  title: string;
+  path: string;
 }
 
-function FooterLink({ href, label }: FooterLinkProps) {
+function FooterMenuItem({ path, title }: Menu) {
   return (
     <Link
-      href={href}
-      className='text-lg flex
-        mobile:w-[81px] mobile:px-[10px] 
-        tablet:w-[100px] tablet:px-6 tablet:py-3 tablet:items-center tablet:justify-center
-        desktop:w-[120px] desktop:text-xl
+      key={title}
+      href={path}
+      className='text-lg flex 
+          px-[10px]
+          tablet:px-6 tablet:py-2.5 tablet:items-center tablet:justify-center
+          desktop:px-6 desktop:py-2.5 desktop:text-xl
           '
     >
-      {label}
+      {title}
     </Link>
   );
 }
@@ -29,32 +30,36 @@ function Footer() {
   const [{ width }] = useInnerWidthState();
 
   const links = [
-    { href: '/Home', label: 'Home' },
-    { href: '/About', label: 'About' },
-    { href: '/Pricing', label: 'Pricing' },
-    { href: '/Blog', label: 'Blog' },
-    { href: '/Contact', label: 'Contact' },
+    { path: '/', title: 'Home' },
+    { path: '/aboutus', title: 'About us' },
+    { path: `https://www.instagram.com/animals_near_you/`, title: 'Instagram' },
+    { path: '/Pricing', title: '개인정보처리방침' },
+    { path: '/Blog', title: '대표메일' },
   ];
+
   return (
-    <footer className={`bg-white w-full`}>
-      <div className='flex mobile:mb-[46.5px] tablet:mb-[51px] tablet:items-center desktop:mb-[63px]'>
-        <section
-          className='flex justify-center items-center  
-        mobile:ml-[40px] mobile:mr-[43px] mobile:h-[30px]
-        tablet:ml-[52px] tablet:mr-[40px]
-        desktop:ml-[83px] desktop:mr-[213px]
-        '
+    <footer className='bg-white desktop:px-16'>
+      <div>
+        <div
+          className='flex tablet:flex-col desktop:flex-row w-full px-10 py-16 tablet:px-0
+        gap-[27px] tablet:gap-4 desktop:gap-48  tablet:items-center desktop:justify-center'
         >
-          <Logo size={width > 1439 ? 'lg' : 'sm'} />
-        </section>
-        <nav className='flex flex-wrap font-normal text-theme-main_dark mobile:gap-1 mobile:items-start tablet:gap-0 tablet:items-center tablet:justify-center desktop:gap-4 '>
-          {links.map((link) => (
-            <FooterLink key={link.href} href={link.href} label={link.label} />
-          ))}
-        </nav>
+          <section className='flex justify-center items-center h-[30px]'>
+            <Logo size={width > 1439 ? 'lg' : 'sm'} />
+          </section>
+          <nav className='flex flex-wrap font-normal text-theme-main_dark mobile:gap-1 mobile:items-start tablet:gap-2 tablet:items-center tablet:justify-center desktop:gap-4 '>
+            {links.map((link) => (
+              <FooterMenuItem
+                key={link.title}
+                title={link.title}
+                path={link.path}
+              />
+            ))}
+          </nav>
+        </div>
       </div>
-      <hr className='border border-theme-main w-full mobile:mb-[22.5px] tablet:mb-[51px] desktop:mb-[63px] ' />
-      <section className='w-full text-center'>
+      <section className='flex flex-col w-full gap-[22.5px] tablet:gap-[2.5625rem] desktop:gap-[3.9375rem] text-center'>
+        <hr className='w-full border border-theme-main' />
         <Copyright size={width > 767 ? 'lg' : 'sm'} />
       </section>
     </footer>
