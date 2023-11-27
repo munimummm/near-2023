24b6317219113e20830de96caec8aaa38df857a5
syssys131 <syssys131@gmail.com>
2023-11-27 20:47:04 +0900

    [chore] storybook stories 경로 수정

--- apps/storybook/src/stories/buttons/Button.stories.ts
+++ apps/storybook/src/stories/buttons/Button.stories.ts
@@ -4,7 +4,7 @@ import Button from 'ui/components/buttons/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Components/Button',
+  title: 'Components/Buttons/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
