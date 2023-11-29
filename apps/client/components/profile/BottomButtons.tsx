438deba6afe37ccfe87ace7f8e7343c98282e955
jurney <jurneyx2@gmail.com>
2023-11-29 22:48:41 +0900

    [fix, chore] mypage 폴더 내 페이지들 profile 폴더로 이동, profile 내 버튼에 라우팅 추가

--- apps/client/components/profile/BottomButtons.tsx
+++ apps/client/components/profile/BottomButtons.tsx
@@ -14,9 +14,9 @@ function BottomButton({ href, text }) {
 function BottomButtonsSection() {
   return (
     <section className='w-full flex flex-col justify-center items-center gap-8 mobile:flex-col mobile:gap-8 tablet:flex-row tablet:gap-20 desktop:flex-row desktop:gap-[7.5rem]'>
-      <BottomButton href={'/'} text={'비밀번호 변경'} />
+      <BottomButton href={'/profile/passwordchange'} text={'비밀번호 변경'} />
       <div className='bg-gray-1 w-[22.5rem] h-[0.0625rem] mobile:w-[22.5rem] mobile:h-[0.0625rem] tablet:w-[0.0625rem] tablet:h-[3.0313rem] desktop:w-[0.0625rem] desktop:h-[3.0313rem]' />
-      <BottomButton href={'/'} text={'회원 탈퇴'} />
+      <BottomButton href={'/profile/withdraw'} text={'회원 탈퇴'} />
     </section>
   );
 }
