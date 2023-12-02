f4c971b245ab538c67b237e171445de769db68ea
jurney <jurneyx2@gmail.com>
2023-12-03 01:03:49 +0900

    [fix] passwordchange, personalchange 내 상대경로 라우팅 수정, profile 내 라우팅 수정

--- apps/client/app/profile/page.tsx
+++ apps/client/app/profile/page.tsx
@@ -6,12 +6,17 @@ import BottomButtonsSection from '../../components/profile/BottomButtons';
 import { useRouter } from 'next/navigation';
 import ProfileBoxSection from '../../components/profile/ProfileBoxSection';
 import CardsSection from '../../components/profile/CardsSection';
-import { Session, createClientComponentClient } from '@near/supabase';
+import {
+  PostgrestSingleResponse,
+  Session,
+  createClientComponentClient,
+} from '@near/supabase';
 
 export default function ProfilePage() {
   const router = useRouter();
   const supabase = createClientComponentClient();
   const [userSession, setuserSession] = useState<Session | null>();
+  const [profile, setProfile] = useState<any[] | null>();
 
   async function getUserSession() {
     const { data, error } = await supabase.auth.getSession();
@@ -22,23 +27,39 @@ export default function ProfilePage() {
       console.log(error);
     }
   }
-
-  if (userSession === null) {
-    router.push('/');
+  async function getLoginedUserProfile() {
+    const { data } = await supabase.from('user_profile').select('*');
+    setProfile(data);
   }
 
   useEffect(() => {
     getUserSession();
+    getLoginedUserProfile();
 
+    if (userSession === null) {
+      router.push('/');
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  useEffect(() => {}, [profile]);
 
   return (
     <>
       {userSession ? <Top /> : <TopSuspense />}
       <main className='flex flex-col gap-[7.5rem] mobile:gap-[7.5rem] tablet:pt-[6.5625rem] tablet:gap-[11.5625rem] desktop:pt-[8.125rem] desktop:gap-[20.0625rem]'>
         <section className='flex flex-col gap-[7.5rem] justify-center items-center mobile:flex-col tablet:flex-col desktop:flex-row'>
-          <ProfileBoxSection />
+          {profile?.map((item, idex) => {
+            return (
+              <ProfileBoxSection
+                key={idex}
+                href={item.id.substr(0, 4)}
+                src={'/images/profile/img_profile_default.png'}
+                level={item.level}
+                name={item.name}
+                email={item.email}
+              />
+            );
+          })}
           <CardsSection />
         </section>
         <BottomButtonsSection />
--- apps/client/components/profile/ProfileBoxSection.tsx
+++ apps/client/components/profile/ProfileBoxSection.tsx
@@ -1,28 +1,30 @@
 import { ModifyPet, VerticalProfile } from 'ui';
 import { DummyPetProfile, DummyProfile } from './dummy';
 
-// interface ProfileBoxSectionProps {
-//   href: string;
-//   src: '/images/profile/img_profile_default.png';
-// }
-function ProfileBoxSection() {
-  const data = DummyProfile;
+interface ProfileBoxSectionProps {
+  href: string;
+  src: '/images/profile/img_profile_default.png';
+  level: string;
+  name: string;
+  email: string;
+}
+function ProfileBoxSection({ ...props }: ProfileBoxSectionProps) {
   const petData = DummyPetProfile;
-
+  const { src, level = 1, name, email } = props;
   return (
     <section className='flex flex-col gap-12 items-center justify-center rounded-none mobile:rounded-none tablet:rounded-[1.25rem] desktop:rounded-[1.25rem] bg-transparent mobile:bg-transparent p-0 mobile:p-0 tablet:p-6 tablet:pb-[4.5rem] tablet:bg-gray-3 desktop:p-6 desktop:pb-[4.5rem] desktop:bg-gray-3'>
       <VerticalProfile
-        href={`/profile/${data.id}`}
-        src={data.src}
-        level={data.level}
-        name={data.name}
-        email={data.email}
+        href={`/profile/personalchange`}
+        src={src}
+        level={Number(level)}
+        name={name}
+        email={email}
       />
       {/* <AddPet backgroundColor='white' href={'/profile/pet'} /> */}
       <ModifyPet
         href={`/profile/pet`}
         name={petData.name}
-        src={petData.src}
+        src={'/images/profile/img_test.jpg'}
         gender={petData.gender}
         age={petData.age}
       />
