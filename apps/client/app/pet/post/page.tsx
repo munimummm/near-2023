5c0ee41a37076c7e682465b3606424272a425a31
syssys131 <syssys131@gmail.com>
2023-12-14 15:45:21 +0900

    [feat] 근처동물 - 임시보호 신청하기 임시 커밋

---
+++ apps/client/app/pet/post/page.tsx
@@ -0,0 +1,19 @@
+import { Cookies } from '@near/react-cookie';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+function NearAnimalPostPage() {
+  const router = useRouter();
+
+  const cookies = new Cookies();
+  useEffect(() => {
+    if (cookies.get('sb-ztcvdzkqqrglziiavupe-auth-token') === undefined) {
+      router.push('/login');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cookies]);
+
+  return <div>등록</div>;
+}
+
+export default NearAnimalPostPage;
