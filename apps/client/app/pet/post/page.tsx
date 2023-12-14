ed4ee673f01845d65100469a6f17bd39b8a4c3fd
syssys131 <syssys131@gmail.com>
2023-12-14 15:53:36 +0900

    [fix] use client 추가

--- apps/client/app/pet/post/page.tsx
+++ apps/client/app/pet/post/page.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import { Cookies } from '@near/react-cookie';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 function NearAnimalPostPage() {
   const router = useRouter();
-
   const cookies = new Cookies();
+
   useEffect(() => {
     if (cookies.get('sb-ztcvdzkqqrglziiavupe-auth-token') === undefined) {
       router.push('/login');
