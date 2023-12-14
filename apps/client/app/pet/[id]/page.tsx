c6c73f07c1e1edcfebb86881ab4a55e5d3bef846
syssys131 <syssys131@gmail.com>
2023-12-14 16:00:15 +0900

    [fix] supabase 관련 코드 주석 처리

--- apps/client/app/pet/[id]/page.tsx
+++ apps/client/app/pet/[id]/page.tsx
@@ -1,31 +1,32 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
-import { Session, createClientComponentClient } from '@near/supabase';
-import { Breadcrumb, Footer, Top, TopSuspense } from 'ui';
+// import { useEffect, useState } from 'react';
+// import { Session, createClientComponentClient } from '@near/supabase';
+import { Breadcrumb, Footer, Top } from 'ui';
 
 function NearAnimalDetailPage({ params }: { params: { id: number } }) {
-  const supabase = createClientComponentClient();
-  const [userSession, setUserSession] = useState<Session | null>();
+  // const supabase = createClientComponentClient();
+  // const [userSession, setUserSession] = useState<Session | null>();
 
-  async function getUserSession() {
-    const { data, error } = await supabase.auth.getSession();
-    if (data) {
-      setUserSession(data.session);
-    }
-    if (error) {
-      console.log(error);
-    }
-  }
+  // async function getUserSession() {
+  //   const { data, error } = await supabase.auth.getSession();
+  //   if (data) {
+  //     setUserSession(data.session);
+  //   }
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  useEffect(() => {
-    getUserSession();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   getUserSession();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <>
-      {userSession ? <Top /> : <TopSuspense />}
+      <Top />
+      {/* {userSession ? <Top /> : <TopSuspense />} */}
       <main className='flex flex-col items-center mt-[2.9375rem] pb-[7.0625rem] mobile:mt-[2.9375rem] mobile:pb-[7.0625rem] tablet:mt-[2.9375rem] tablet:pb-[18.9375rem] desktop:mt-0 desktop:pb-[15.8125rem]'>
         <Breadcrumb
           className='w-full pl-4 desktop:pl-10'
--- apps/client/app/pet/page.tsx
+++ apps/client/app/pet/page.tsx
@@ -1,34 +1,34 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { Session, createClientComponentClient } from '@near/supabase';
-import { Footer, Top, TopSuspense } from 'ui';
+// import { useEffect, useState } from 'react';
+// import { Session, createClientComponentClient } from '@near/supabase';
+import { Footer, Top } from 'ui';
 import NearAnimalCardSection from '../../components/pet/CardSection';
 import { TopData } from '../../components/home/dummy';
 import TopCarousel from '../../components/home/TopCarousel';
 
 function NearAnimalPage() {
-  const supabase = createClientComponentClient();
-  const [userSession, setUserSession] = useState<Session | null>();
+  // const supabase = createClientComponentClient();
+  // const [userSession, setUserSession] = useState<Session | null>();
 
-  async function getUserSession() {
-    const { data, error } = await supabase.auth.getSession();
-    if (data) {
-      setUserSession(data.session);
-    }
-    if (error) {
-      console.log(error);
-    }
-  }
+  // async function getUserSession() {
+  //   const { data, error } = await supabase.auth.getSession();
+  //   if (data) {
+  //     setUserSession(data.session);
+  //   }
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  useEffect(() => {
-    getUserSession();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   getUserSession();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <>
-      {userSession ? <Top /> : <TopSuspense />}
+      <Top />
       <main className='flex flex-col items-center mt-[2.9375rem] pb-[7.0625rem] mobile:mt-[2.9375rem] mobile:pb-[7.0625rem] tablet:mt-[2.9375rem] tablet:pb-[18.9375rem] desktop:mt-0 desktop:pb-[15.8125rem]'>
         {/* home과 동일 */}
         <TopCarousel slides={TopData} />
