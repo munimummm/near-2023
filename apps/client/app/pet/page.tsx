9622c626a3e89aa8c268aa127d43636b205f5bae
syssys131 <syssys131@gmail.com>
2023-12-14 15:43:30 +0900

    [feat] 근처동물 - 홈 임시 커밋

---
+++ apps/client/app/pet/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Session, createClientComponentClient } from '@near/supabase';
+import { Footer, Top, TopSuspense } from 'ui';
+import NearAnimalCardSection from '../../components/pet/CardSection';
+import { TopData } from '../../components/home/dummy';
+import TopCarousel from '../../components/home/TopCarousel';
+
+function NearAnimalPage() {
+  const supabase = createClientComponentClient();
+  const [userSession, setUserSession] = useState<Session | null>();
+
+  async function getUserSession() {
+    const { data, error } = await supabase.auth.getSession();
+    if (data) {
+      setUserSession(data.session);
+    }
+    if (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getUserSession();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {userSession ? <Top /> : <TopSuspense />}
+      <main className='flex flex-col items-center mt-[2.9375rem] pb-[7.0625rem] mobile:mt-[2.9375rem] mobile:pb-[7.0625rem] tablet:mt-[2.9375rem] tablet:pb-[18.9375rem] desktop:mt-0 desktop:pb-[15.8125rem]'>
+        {/* home과 동일 */}
+        <TopCarousel slides={TopData} />
+        <NearAnimalCardSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default NearAnimalPage;
