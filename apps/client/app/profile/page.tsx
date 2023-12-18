cf9c8e3c3efbf00204aa80754590efe1c4d4e7f5
munimummm <moom6857@naver.com>
2023-12-18 10:48:29 +0900

    [chore] 민욱님 페이지 eslint deps line 추가

--- apps/client/app/profile/page.tsx
+++ apps/client/app/profile/page.tsx
@@ -37,6 +37,7 @@ export default function ProfilePage() {
         setSession(session.session);
       }
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const cookies = new Cookies();
