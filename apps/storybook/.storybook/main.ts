6a0c22a42411549cfebd93b817b254508c1878e2
jurney <jurneyx2@gmail.com>
2023-12-19 23:11:17 +0900

    [fix] workflows/chromatic.yml 수정

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -38,7 +38,8 @@ const config: StorybookConfig = {
   env: (config) => ({
     ...config,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
   }),
   webpackFinal: async (config) => {
     const imageRule = config.module?.rules?.find((rule) => {
