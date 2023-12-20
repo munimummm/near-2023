2e223415403d8197d05cc67ab0337687560f69c1
OhMinWook <minugi72@gmail.com>
2023-12-20 13:19:50 +0900

    [fix] px changes into rem

--- apps/client/app/newsletter/page.tsx
+++ apps/client/app/newsletter/page.tsx
@@ -78,10 +78,10 @@ const NewsletterHome = () => {
     <div>
       <section className='layout_max_width'>
         <div>{userSession ? <Top /> : <TopSuspense />}</div>
-        <div className='mt-[100px] pl-[40px] mobile:mb-[30px] desktop:mb-[77px]'>
+        <div className='mt-[6.25rem] pl-[2.5rem] mobile:mb-[1.875rem] desktop:mb-[4.8125rem]'>
           <Breadcrumb items={['뉴스레터', '홈']} />
         </div>
-        <div className='relative bg-slate-300 mobile:mb-[70px]'>
+        <div className='relative bg-slate-300 mobile:mb-[4.375rem]'>
           <Image
             src={'/images/newsletter/header_image.png'}
             alt={'등록된 사진이 존재하지 않습니다.'}
@@ -90,10 +90,10 @@ const NewsletterHome = () => {
             height={500}
           />
           <div className='absolute bottom-0 left-5 desktop:bottom-0'>
-            <div className='text-white font-bold mobile:text-[24px] pb-[10px]'>
+            <div className='text-white font-bold mobile:text-[1.5rem] pb-[0.625rem]'>
               NEAR 뉴스레터
             </div>
-            <div className='flex flex-col w-[300px] h-[79px]'>
+            <div className='flex flex-col w-[18.75rem] h-[4.9375rem]'>
               <span className='text-xs text-white'>
                 우리는 어쩌면 유기견에 대해 잘 모를 수도 있어요
               </span>
@@ -110,7 +110,7 @@ const NewsletterHome = () => {
           </div>
         </div>
         <section className=''>
-          <div className='mobile:w-[280px] mobile:ml-[32px] tablet:w-[380px] desktop:w-[400px]'>
+          <div className='mobile:w-[17.5rem] mobile:ml-[2rem] tablet:w-[23.75rem] desktop:w-[25rem]'>
             <TextInput
               state='search'
               name={'search'}
@@ -119,8 +119,8 @@ const NewsletterHome = () => {
               placeholder='검색어를 입력하세요'
             />
           </div>
-          <div className='flex flex-row justify-between h-[74px] pt-[12px] px-[32px] mb-[48px] border-b-4'>
-            <div className='font-bold text-[20px]'>
+          <div className='flex flex-row justify-between h-[4.625rem] pt-[0.75rem] px-[2rem] mb-[3rem] border-b-4'>
+            <div className='font-bold text-[1.25rem]'>
               NEAR가 추천하는 뉴스레터
             </div>
             <button className='text-s' onClick={onClickList}>
@@ -132,7 +132,7 @@ const NewsletterHome = () => {
             {/* {visible === true ? null : ( */}
             <button
               type='button'
-              className='mt-[150px] w-[470px] h-[60px] bg-slate-200 rounded-lg flex items-center pl-[20px] tablet:hidden'
+              className='mt-[9.375rem] w-[29.375rem] h-[3.75rem] bg-slate-200 rounded-lg flex items-center pl-[1.25rem] tablet:hidden'
               onClick={onClickMore}
             >
               더 많은 뉴스레터 보기
@@ -144,7 +144,7 @@ const NewsletterHome = () => {
             <Pagination total={5} />
           </div> */}
         </section>
-        <div className='mt-[200px]'>
+        <div className='mt-[12.5rem]'>
           <Footer />
         </div>
       </section>
